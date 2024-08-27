2e7d353482b035f475318020139f8fe62087106d
eshasahu-21 <sahuesha882@gmail.com>
2024-08-27 18:28:31 +0530

    Updated PrePostCondition file

--- PrePostCondition.java
+++ PrePostCondition.java
@@ -5,18 +5,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class PrePostCondition {
 	public static void main(String[]args)throws InterruptedException {
 	WebDriver driver=new ChromeDriver();
+	
+	// facebook
 	//pre condition
 	driver.manage().window().maximize();
 	Thread.sleep(2000);
+	
 	driver.get("https://www.facebook.com/");
 	Thread.sleep(2000);
+	
 	driver.manage().window().fullscreen();
 	Thread.sleep(2000);
+	
 	//post condition
 	driver.manage().window().minimize();
 	
+	// flipkart 
+	// pre condition
+	driver.manage().window().maximize();
+	Thread.sleep(2000);
 	
+	driver.get("https://www.flipkart.com/");
+	Thread.sleep(2000);
 	
-
-}
+	driver.manage().window().fullscreen();
+	Thread.sleep(2000);
+	
+	//post condition
+	driver.manage().window().minimize();
+	}
 }
